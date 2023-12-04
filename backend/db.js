b48cbc79d4dbb5fae9807eae0dc01fb4a0dcdbe1
Sandeep Shrestha <sandeepshrestha288@gmail.com>
2023-12-05 00:24:27 +0545

    alternatives added

--- backend/db.js
+++ backend/db.js
@@ -17,4 +17,14 @@ const connectToMongo = () => {
   });
 };
 
+
+// connectToMongo().catch((err) => console.log(err));
+
+// async function connectToMongo() {
+//   await mongoose.connect("mongodb://127.0.0.1:27017");
+//   console.log("Connected to MongoDB Successfully");
+
+//   // use `await mongoose.connect('mongodb://user:password@127.0.0.1:27017/test');` if your database has auth enabled
+// }
+
 module.exports = connectToMongo;
