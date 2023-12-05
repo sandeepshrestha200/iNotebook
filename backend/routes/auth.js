7a6bb809fbc19a98916dbb9c6932b20c1decad62
Sandeep Shrestha <sandeepshrestha288@gmail.com>
2023-12-06 00:19:39 +0545

    storing usser data using auth.js(post) in db #45

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,12 +1,13 @@
 const express = require("express");
+const User = require("../models/User");
 const router = express.Router();
 
-router.get("/", (req, res) => {
-  var obj = {
-    path: "/auth",
-    port: 3000,
-  };
-  res.json(obj);
+// Create a User using: POST "/api/auth/". Doesn't require Auth
+router.post("/", (req, res) => {
+  console.log(req.body);
+  const user = User(req.body);
+  user.save();
+  res.send("Hello");
 });
 
 module.exports = router;
