c9865d27732c9ad7e3349a9ee28167fb9cb800e2
Sandeep Shrestha <sandeepshrestha288@gmail.com>
2023-12-07 20:03:35 +0545

    created middleware and decode JWT for get user #51

---
+++ backend/middleware/fetchuser.js
@@ -0,0 +1,25 @@
+const jwt = require("jsonwebtoken");
+const JWT_SERECT = "IAmLearningReactJSNow.";
+
+const fetchuser = (req, res, next) => {
+  // Get the user from the jwt token and add id to req object
+  const token = req.header("access-token");
+  if (!token) {
+    res.status(401).send({ error: "Please authenticate using valid token." });
+  }
+  try {
+    const data = jwt.verify(token, JWT_SERECT);
+    req.user = data.user;
+    next();
+  } catch (error) {
+    res.status(401).send({ error: "Please authenticate using valid token." });
+      
+  }
+};
+
+module.exports = fetchuser;
+
+/*  {
+  "message": "User Logged in successfully.",
+  "accessToken": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjp7ImlkIjoiNjU3MWJhNzQyZmNiYThlMTVjNWI2ZTVjIn0sImlhdCI6MTcwMTk1NTQ0Nn0.E9W0ZQ0VLEFeJYO0Gmxfsp4bAl5d5NQggx5QcD_ys3Q"
+} */
--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const User = require("../models/User");
+const fetchuser = require("../middleware/fetchuser");
 const router = express.Router();
 const { body, validationResult } = require("express-validator");
 const bcrypt = require("bcryptjs");
@@ -7,7 +8,7 @@ const jwt = require("jsonwebtoken");
 
 const JWT_SERECT = "IAmLearningReactJSNow.";
 
-// Create a User using: POST "/api/auth/createuser/". Doesn't require Auth
+// ROUTE 1 : Create a User using: POST "/api/auth/createuser/". Doesn't require Auth
 router.post(
   "/createuser",
   [
@@ -56,7 +57,7 @@ router.post(
   }
 );
 
-// Authenticate a User using: POST "/api/auth/login". Doesn't require Auth
+// ROUTE 2 :  Authenticate a User using: POST "/api/auth/login". Required Auth
 router.post("/login", [body("email", "Enter a valid email.").isEmail(), body("password", "Password cannot be empty).").exists()], async (req, res) => {
   // returning errors for bad requests
   const errors = validationResult(req);
@@ -85,6 +86,18 @@ router.post("/login", [body("email", "Enter a valid email.").isEmail(), body("pa
     const accessToken = jwt.sign(data, JWT_SERECT);
 
     res.json({ message: "User Logged in successfully.", accessToken });
+  } catch (error) {
+    // console.error(error.message);
+    res.status(500).send("Internal Server Error.");
+  }
+});
+
+// ROUTE 2 :  Get Logged in Userdetails using: POST "/api/auth/getuser". Required Auth
+router.post("/getuser", fetchuser, async (req, res) => {
+  try {
+    const userId = req.user.id;
+    const user = await User.findById(userId).select("-password");
+    res.json({ user });
   } catch (error) {
     // console.error(error.message);
     res.status(500).send("Internal Server Error.");
