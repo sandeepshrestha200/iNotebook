e99b2d4a8b39605a876a87aee3db7da7414182b2
Sandeep Shrestha <sandeepshrestha288@gmail.com>
2023-12-11 09:11:14 +0545

    Navbar and routes created #56

--- src/App.js
+++ src/App.js
@@ -1,10 +1,27 @@
-import './App.css';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import "./App.css";
+import Navbar from "./components/Navbar";
+import Home from "./components/Home";
+import About from "./components/About";
+import Note from "./components/Note";
 
 function App() {
   return (
-    <div className="App">
-      <h1>This is iNotebook.</h1>
-    </div>
+    <>
+      <BrowserRouter>
+        <Navbar />
+
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+          <Route exact path="/about" element={<About />} />
+          <Route exact path="/note" element={<Note />} />
+        </Routes>
+
+        <div className="App">
+          <h1>This is iNotebook.</h1>
+        </div>
+      </BrowserRouter>
+    </>
   );
 }
 
---
+++ src/components/Note.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const Note = () => {
+  return <div>This is Note page.</div>;
+}
+
+export default Note
