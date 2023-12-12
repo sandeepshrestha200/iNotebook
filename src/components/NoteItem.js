7550e1ddc7eb46ca5a5ac0aa55cfe693fbfb56fa
Sandeep Shrestha <sandeepshrestha288@gmail.com>
2023-12-12 23:08:31 +0545

    icon section refactored

--- src/components/NoteItem.js
+++ src/components/NoteItem.js
@@ -11,12 +11,12 @@ const NoteItem = (props) => {
       <div className="col-md-3 my-3">
         <div className="card">
           <div className="card-body">
-            <div className="d-flex align-items-center">
-              <h5 className="card-title">{note.title}</h5>
-              <FaTrashAlt className="mx-2 icon" />
-              <FaEdit className="mx-2 icon" />
-            </div>
+            <h5 className="card-title">{note.title}</h5>
             <p className="card-text">{note.description}</p>
+            <div className="d-flex align-items-center justify-content-center fs-5 icon-container mx-5 py-2 rounded-pill">
+              <FaTrashAlt className="mx-2 text-danger icon" />
+              <FaEdit className="mx-2 text-success icon" />
+            </div>
           </div>
         </div>
       </div>
